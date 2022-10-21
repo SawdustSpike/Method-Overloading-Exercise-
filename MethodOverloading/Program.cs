8e75dea3c07ef0235737f77c7b95a2453b9a84bf
Mike Cowell <spike@spikeandbetty.com>
2022-10-21 09:28:53 -0400

    init commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,29 @@
 {
     public class Program
     {
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        static string Add(int x, int y, bool z)
+        {
+            if (z = true && (x + y > 1))
+                return $"{x + y} dollars";
+            else if (z = true && x + y == 1)
+                return $"{x + y} dollar";
+            else
+                return "Something has gone wrong";
+                            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 5));
+            Console.WriteLine(Add(1.3, 6.3));
+            Console.WriteLine(Add(2, 3, true));
+
         }
     }
 }
